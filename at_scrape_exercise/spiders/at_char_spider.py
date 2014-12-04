4ee7460f6214516f6dfe9c6c0a9c81a3ba35ea9c
mathhomework <toxstalks@gmail.com>
2014-12-03 23:43:11 -0800

    overall char_detail works

--- at_scrape_exercise/spiders/at_char_spider.py
+++ at_scrape_exercise/spiders/at_char_spider.py
@@ -43,10 +43,20 @@ class AT_Char_Spider_Detail(Spider):
     # start_urls = [url.strip() for url in characters.readlines()]
     start_urls = [
         # "http://adventuretime.wikia.com/wiki/Doctor_Princess",
-        "http://adventuretime.wikia.com/wiki/Finn",
-        "http://adventuretime.wikia.com/wiki/Marceline",
-        "http://adventuretime.wikia.com/wiki/Breakfast_Princess",
-        "http://adventuretime.wikia.com/wiki/Abe_Lincoln",
+        # "http://adventuretime.wikia.com/wiki/Finn",
+        # "http://adventuretime.wikia.com/wiki/Jay_%26_Bonnie",
+        # "http://adventuretime.wikia.com/wiki/Marceline",
+        # # "http://adventuretime.wikia.com/wiki/Breakfast_Princess",
+        # "http://adventuretime.wikia.com/wiki/Abe_Lincoln",
+        # "http://adventuretime.wikia.com/wiki/Ice_King",
+        # "http://adventuretime.wikia.com/wiki/Ricardio",
+        # "http://adventuretime.wikia.com/wiki/Mr._Pig",
+        # "http://adventuretime.wikia.com/wiki/Earl_of_Lemongrab",
+        # "http://adventuretime.wikia.com/wiki/Princess_Bubblegum",
+        "http://adventuretime.wikia.com/wiki/Lich_King",
+
+
+
     ]
 
     def parse(self, response):
@@ -54,38 +64,50 @@ class AT_Char_Spider_Detail(Spider):
         sel = Selector(response)
         data = sel.xpath("//table[@class='infobox']")
         categories = data.xpath("tr[position()>2]/td/b/text()").extract()
-        link = self.start_urls[0]
 
-        species = data.xpath("tr/td/a[../../td/b/text()='Species']/text()|tr[td/b/text()='Species']/td/text()[normalize-space()]").extract()
-        print species
+        species_list = data.xpath("tr/td/a[../../td/b/text()='Species']/text()|tr[td/b/text()='Species']/td/text()[normalize-space()]").extract()
+        species = [x.strip() for x in species_list]
         # returns [u'Vampire', u'Demon']
-
+        print "*************SPECIES**********"
+        print species
         # the occupation below does not take into account a tags... so Marceline's Henchmen would just be something like 's henchmen
         # occupation = data.xpath("tr[td/b[contains(.,'Occupation')]]/td[position()>1]/text()").extract()
         # occupation = data.xpath("tr[td/b[contains(.,'Occupation')]]/td[position()>1]/descendant::text()").extract()
         # this is more specific than the below because it does not matter where the extra space after occupation is
         # occupation = data.xpath("tr[td/b/text()='Occupation ']/td[position()>1]/text()").extract()
-        occupation = data.xpath("tr[td/b[contains(.,'Occupation')]]/td[position()>1]").extract()[0]
+        try:
+            occupation = data.xpath("tr[td/b[contains(.,'Occupation')]]/td[position()>1]").extract()[0]
 
-        print "***********OCCUPATION******************"
-        for x in str(occupation).split("<br>"):
-            y = re.sub('<[^>]*>', '', re.sub('\(.*?\)', '', x)).strip().rstrip(",")
-            print y
-        print "**********END OCCUPATION**************"
+            print "***********OCCUPATION******************"
+            for x in str(occupation).split("<br>"):
+                y = re.sub('<[^>]*>', '', re.sub('\(.*?\)', '', x)).strip().rstrip(",")
+                print y
+            print "**********END OCCUPATION**************"
+        except IndexError:
+            pass
 
         sex = data.xpath("normalize-space(tr[td/b/text()='Sex']/td[position()>1]/text())").extract()
+        print '**********SEX**********'
         print sex
-
         name = data.xpath("normalize-space(tr[td/b/text()='Name']/td[position()>1]/text())").extract()
+        print "**********NAME****************"
         print name
-
-        relatives = data.xpath("tr[td/b/text()='Relatives']/td[position()>1]/a/text()").extract()
+        print "************relatives**************"
+        # relatives = data.xpath("tr[td/b/text()='Relatives']/td[position()>1]/a/text()").extract()
+        relatives = data.xpath("tr[td/b/text()='Relatives']/td[position()>1]/descendant::a/text()[not(ancestor::small)]").extract()
+        # relatives = str(data.xpath("tr[td/b/text()='Relatives']/td[position()>1]").extract())
         print relatives
-        print "WOWLOWLWOWLWOLWOW"
-        link = response.request.url
-        print link
 
-        appearances = sel.xpath("//div[@id='mw-content-text']/ul[1]/li/a/text()").extract()
+        link = response.request.url
+        print "************Link*****************"
+        print link
+        print "*************appearances*********"
+        # This version makes sure that if a minor character is being scraped, it will return an empty list if there's no Episodes appearances section
+        body_appearances = sel.xpath("//div[@id='mw-content-text']/*[self::h3 or self::h2][span[@id='Major_appearances' or @id='Minor_appearances' or @id='Episode_appearances']]/following-sibling::*[1]/li/a/text()").extract()
+        sidebar_introduced = data.xpath("normalize-space(tr[td/b/text()='Introduced in']/td[position()>1]/a/text())").extract()
+        appearances = body_appearances or sidebar_introduced
         print appearances
+
         image = data.xpath("tr/td/a[@class='image image-thumbnail']/@href").extract()
+        print "***********IMAGE********"
         print image
