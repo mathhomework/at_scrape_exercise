8fa18a2bc9ca4e95e139fe4bb00c822ef8f64ce3
mathhomework <toxstalks@gmail.com>
2014-12-03 18:51:45 -0800

    tiny cleanup

--- at_scrape_exercise/spiders/at_char_spider.py
+++ at_scrape_exercise/spiders/at_char_spider.py
@@ -49,16 +49,12 @@ class AT_Char_Spider_Detail(Spider):
         "http://adventuretime.wikia.com/wiki/Abe_Lincoln",
     ]
 
-
     def parse(self, response):
 
         sel = Selector(response)
         data = sel.xpath("//table[@class='infobox']")
         categories = data.xpath("tr[position()>2]/td/b/text()").extract()
-        # info = data.xpath("tr[position()>2]/td/text()|//table[@class='infobox']/tr[position()>2]/td/a/text()|//table[@class='infobox']/tr[position()>2]/td/b/a/text()").extract()
         link = self.start_urls[0]
-        # info = [x for x in info if len(x) > 2]
-
 
         species = data.xpath("tr/td/a[../../td/b/text()='Species']/text()|tr[td/b/text()='Species']/td/text()[normalize-space()]").extract()
         print species
@@ -89,36 +85,7 @@ class AT_Char_Spider_Detail(Spider):
         link = response.request.url
         print link
 
-        # print "INFO***********"
-        # print info
-        # print "=================="
-
-        # sometimes data has spaces in front or new lines escape char at the end, so this removes it.
-        # commented out because it was referring to unused info. use this for new dict data.
-        # for x in range(len(info)):
-        #     if info[x][0] == " ":
-        #         info[x] = info[x][1:]
-        #     if "\n" in info[x]:
-        #         info[x] = info[x][:-1]
-
-        # combined_info = dict(zip(categories, info))
-        #
         appearances = sel.xpath("//div[@id='mw-content-text']/ul[1]/li/a/text()").extract()
         print appearances
         image = data.xpath("tr/td/a[@class='image image-thumbnail']/@href").extract()
         print image
-        # name = combined_info["Name"]
-        # if "Species" in combined_info:
-        #     species = combined_info["Species"]
-        # if "Sex" in combined_info:
-        #     sex = combined_info["Sex"]
-        # if "Introduced in" in combined_info:
-        #     introduced = combined_info["Introduced in"]
-        # print "************************"
-        # print name
-        # print species
-        # print sex
-        # print introduced
-        # print image
-        # print "************************"
-        # print combined_info
